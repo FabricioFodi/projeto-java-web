9d326a939e0474f0c72b449715ca1205bc50a53b
Fabricio Fodi <fabricio_fodi@estudante.sc.senai.br>
2025-04-22 17:17:04 -0300

    update

--- frontend/projeto_java_web/src/pages/main/mainPage.tsx
+++ frontend/projeto_java_web/src/pages/main/mainPage.tsx
@@ -32,10 +32,10 @@ function Main() {
                     throw new Error('Erro na resposta do servidor');
                 }
                 setUsers(data);
-                toast.success('Conexão com o banco de dados bem sucedida', {autoClose: 3000});
+                toast.success('Conexão com o banco de dados bem sucedida', { autoClose: 3000 });
             }
             catch (erro) {
-                toast.error('Erro ao conectar com o banco de dados', {autoClose: 3000});
+                toast.error('Erro ao conectar com o banco de dados', { autoClose: 3000 });
             }
         };
 
@@ -84,35 +84,38 @@ function Main() {
 
     return (
         <>
-            <div className='tudo'>
+            <div className='border 1px solid black'>
                 <h1>Lista de Usuários CRUD</h1>
-                <div className="tabela">
-                    <table ref={tableRef}>
-                        <thead>
-                            <tr>
-                                {['ID', 'Nome', 'E-mail', 'Telefone', 'Endereço', 'Hobby', 'Ações'].map((col, index) => (
-                                    <th key={index}>
-                                        {col}
-                                        <div className='resizer'></div>
-                                    </th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {users.map((user) => (
-                                <tr key={user.id}>
-                                    <td>{user.id}</td>
-                                    <td>{user.nome}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.phone}</td>
-                                    <td>{user.address}</td>
-                                    <td>{user.hobby}</td>
-                                    <td> <Link to={`/editar/${user.id}`}>✏️</Link> <a >❌</a></td>
-                                </tr>
+                <table ref={tableRef} className="w-full border-collapse border border-slate-400">
+                    <thead>
+                        <tr>
+                            {['ID', 'Nome', 'E-mail', 'Telefone', 'Endereço', 'Hobby', 'Ações'].map((col, index) => (
+                                <th
+                                    key={index}
+                                    className="px-2 py-1 border border-gray-300 bg-gray-100 relative select-none"
+                                >
+                                    {col}
+                                    <div className="resizer w-[5px] h-full bg-transparent absolute right-0 top-0 cursor-col-resize hover:bg-gray-500" />
+                                </th>
                             ))}
-                        </tbody>
-                    </table>
-                </div> {/* Fim da div tabela */}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <tr key={user.id}>
+                                <td className="px-2 py-1 border border-gray-300 relative">{user.id}</td>
+                                <td className="px-2 py-1 border border-gray-300 relative">{user.nome}</td>
+                                <td className="px-2 py-1 border border-gray-300 relative">{user.email}</td>
+                                <td className="px-2 py-1 border border-gray-300 relative">{user.phone}</td>
+                                <td className="px-2 py-1 border border-gray-300 relative">{user.address}</td>
+                                <td className="px-2 py-1 border border-gray-300 relative">{user.hobby}</td>
+                                <td className="px-2 py-1 border border-gray-300 relative">
+                                    <Link to={`/editar/${user.id}`}>✏️</Link> <a>❌</a>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
                 <button className='cadastraUser'><Link to='/cadastro'>Cadastrar Usuario</Link></button>
             </div>
         </>
